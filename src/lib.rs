6e184229f276a0bc20fd96bd2663952ab1e699f3
Daryll Ko <daryll.ko@gmail.com>
2022-08-30 13:46:02 +0800

    feat: add `match_literal`

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,27 @@ mod tests {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct Element {
-	name: String,
-	attributes: Vec<(String, String)>,
-	children: Vec<Element>,
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
 }
 
 fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
-	match input.chars().next() {
-		Some('a') => Ok((&input['a'.len_utf8()..], ())),
-		_ => Err(input)
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
+
+// this returns a closure!
+//
+// note the `impl` before the function return type and the `move` before the closure proper
+//
+// ironically, the variability of `expected` makes length extraction nicer to look at!
+
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+	move |input| match input.get(0..expected.len()) {
+		Some (next) if next == expected => Ok((&input[expected.len()..], ())),
+		_ => Err(input),
 	}
 }
