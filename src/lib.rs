e72c17e9cb02ad65f1a1d856262632d6e9299113
Daryll Ko <daryll.ko@gmail.com>
2022-08-30 13:33:22 +0800

    feat: add `the_letter_a`

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,9 @@ struct Element {
 	children: Vec<Element>,
 }
 
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
+	match input.chars().next() {
+		Some('a') => Ok((&input['a'.len_utf8()..], ())),
+		_ => Err(input)
+	}
+}
